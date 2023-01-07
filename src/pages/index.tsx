d9ae97f59aafc0a2466308105da18535a23d55df
thr3a <thr3a@example.com>
2023-01-07 15:43:59 +0900

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Anchor } from '@mantine/core';
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <Anchor href="/page2">Google</Anchor>
+    </>
+  );
+};
+
+export default IndexPage;
